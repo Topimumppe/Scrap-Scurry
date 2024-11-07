ad6dda71338e6f8de719886c71949669c3ab17f0
Topimumppe <topheikki@gmail.com>
2024-11-07 21:35:02 +0200

    baskaa

--- scrap-scurry/scenes/Scurry.cs
+++ scrap-scurry/scenes/Scurry.cs
@@ -3,12 +3,40 @@ using System;
 
 public partial class Scurry : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	public const float Speed = 150.0f;
 	public const float JumpVelocity = -400.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero) // Liikkuminen X-akselilla
+		{
+			velocity.X = direction.X * Speed;
+		}
+		
+		else // Pysähtyminen X-akselilla
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		if (direction != Vector2.Zero) // Liikkuminen Y-akselilla
+		{
+			velocity.Y = direction.Y * Speed;
+		}
+		else // Pysähtyminen X-akselilla
+		{
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+		
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
+
 
 //		// Add the gravity.
 //		if (!IsOnFloor())
@@ -21,26 +49,3 @@ public partial class Scurry : CharacterBody2D
 //		{
 //			velocity.Y = JumpVelocity;
 //		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-		
-		if (direction != Vector2.Zero)
-		{
-			velocity.Y = direction.Y * Speed;
-		}
-		
-		Velocity = velocity;
-		MoveAndSlide();
-	}
-}
