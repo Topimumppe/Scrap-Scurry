fcb81d8ae78c76d7ff06551f773428128146b8fb
Topimumppe <topheikki@gmail.com>
2024-11-09 00:49:49 +0200

    invi ui toggle systeemi

--- scrap-scurry/scripts/Global.cs
+++ scrap-scurry/scripts/Global.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Global : Node
 {
+	private Node inventory_screen = ResourceLoader.Load<PackedScene>("res://scenes/inventory_screen.tscn").Instantiate();
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +13,41 @@ public partial class Global : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (Input.IsActionJustPressed("inventory"))
+		{
+			ToggleInventory();
+		}
+
+	}
+
+	bool is_inventory_open = false;
+	public void OpenInventory()
+	{
+		if (!is_inventory_open)
+		{
+			GetTree().Root.AddChild(inventory_screen);
+			is_inventory_open = true;
+		}
+	}
+
+	public void CloseInventory()
+	{
+		if (is_inventory_open)
+		{
+			GetTree().Root.RemoveChild(inventory_screen);
+			is_inventory_open = false;
+		}
+	}
+
+	public void ToggleInventory()
+	{
+		if (!is_inventory_open)
+		{
+			OpenInventory();
+		}
+		else
+		{
+			CloseInventory();
+		}
 	}
 }
