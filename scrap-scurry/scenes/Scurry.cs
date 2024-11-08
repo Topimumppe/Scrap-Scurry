bc0d62bcc476d93bd035ed799a30a9b0fdd672e5
Topimumppe <topheikki@gmail.com>
2024-11-08 22:16:08 +0200

    tilesetti ja jotai

--- scrap-scurry/scenes/Scurry.cs
+++ scrap-scurry/scenes/Scurry.cs
@@ -13,25 +13,17 @@ public partial class Scurry : CharacterBody2D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero) // Liikkuminen X-akselilla
+		if (direction != Vector2.Zero) // Liikkuminen
 		{
 			velocity.X = direction.X * Speed;
-		}
-		
-		else // Pysähtyminen X-akselilla
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		if (direction != Vector2.Zero) // Liikkuminen Y-akselilla
-		{
 			velocity.Y = direction.Y * Speed;
 		}
-		else // Pysähtyminen X-akselilla
-		{
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
-		}
 		
+		else // Pysähtyminen
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}		
 		Velocity = velocity;
 		MoveAndSlide();
 	}
